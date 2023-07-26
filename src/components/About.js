d869d5784a8ea6c73e3503b925ca12435ce4d351
韓維 <hanwei@hans-mac.lan>
2023-07-26 01:01:43 -0500

    init

--- src/components/About.js
+++ src/components/About.js
@@ -226,7 +226,7 @@ export default function About() {
         <div className="container mx-auto flex px-10 py-20 md:flex-row flex-col items-center" id="result">
             <a href="https://www.instagram.com/reel/CuPDDfUxOGZ/?utm_source=ig_web_copy_link&igshid=MzRlODBiNWFlZA==">
                 <h1 className="title-font sm:text-4xl text-3xl mb-4 font-medium text-white align-content">
-                    {name} You are {score}% gay. <- click me!
+                    {name} You are {score}% gay. &lt!-- click me!
                 </h1>
             </a>
         </div>
