708c58ab9151191c152ea395d4ee8bf81720ded8
韓維 <hanwei@hans-mac.local>
2023-07-25 19:50:51 -0500

    init

--- src/components/About.js
+++ src/components/About.js
@@ -8,8 +8,14 @@ export default function About() {
       <div className="container mx-auto flex px-10 py-20 md:flex-row flex-col items-center">
         <div className="lg:flex-grow md:w-1/2 lg:pr-24 md:pr-16 flex flex-col md:items-start md:text-left mb-16 md:mb-0 items-center text-center">
           <h1 className="title-font sm:text-4xl text-3xl mb-4 font-medium text-white">
-            Hi, want to understand yourself even better? try this questionary.
-            <br className="hidden lg:inline-block" />I love to build amazing
+            Hi, <input
+              type="text"
+              id="name"
+              name="name"
+              className="w-full bg-gray-800 rounded border border-gray-700 focus:border-indigo-500 focus:ring-2 focus:ring-indigo-900 text-base outline-none text-gray-100 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"
+              onChange={(e) => setName(e.target.value)}
+            />
+            <br className="hidden lg:inline-block" /> Is me what I think it is? Want to understand yourself even better? try this questionary.
             apps.
           </h1>
           <p className="mb-8 leading-relaxed">
