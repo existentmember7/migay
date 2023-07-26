a7cdeb9f2918ee069d499bee67c6f8263eb63fd9
韓維 <hanwei@hans-mac.lan>
2023-07-26 02:53:27 -0500

    init

--- src/components/About.js
+++ src/components/About.js
@@ -292,15 +292,15 @@ export default function About() {
                 </label>
         </h1>
         </div>
-        <div className="container mx-auto flex px-10 py-20 md:flex-row flex-col items-center text-center">
+        <div className="lg:flex-grow md:w-1/2 lg:pr-24 md:pr-16 flex flex-col md:items-start md:text-left mb-16 md:mb-0 items-center text-center">
             <button
                 type="submit"
-                className="text-white bg-indigo-500 focus:outline-none hover:bg-indigo-600 rounded text-lg text-center"
+                className="text-white bg-indigo-500 border-0 py-2 px-6 focus:outline-none hover:bg-indigo-600 rounded text-lg justify-center"
                 onClick={(e)=>handleSubmit(e)}>
                 Submit
             </button>
         </div>
-        <div className="container mx-auto flex  md:flex-row flex-col items-center text-center">
+        <div className="container mx-auto flex px-10 py-20 md:flex-row flex-col items-center" id="result">
             <a href="https://www.instagram.com/reel/CuPDDfUxOGZ/?utm_source=ig_web_copy_link&igshid=MzRlODBiNWFlZA==">
                 <h1 className="title-font sm:text-4xl text-3xl mb-4 font-medium text-white justify-center">
                     {name} You are {score}% gay. (click me!)
