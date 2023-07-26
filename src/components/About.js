2d3e5eb2bcc8f7df9732bff9870043634c103684
韓維 <hanwei@hans-mac.lan>
2023-07-26 00:54:21 -0500

    init

--- src/components/About.js
+++ src/components/About.js
@@ -218,16 +218,17 @@ export default function About() {
         <div className="container mx-auto flex px-10 py-20 md:flex-row flex-col items-center">
             <button
                 type="submit"
-                className="text-white bg-indigo-500 border-0 py-2 px-6 focus:outline-none hover:bg-indigo-600 rounded text-lg items-center"
+                className="text-white bg-indigo-500 border-0 py-2 px-6 focus:outline-none hover:bg-indigo-600 rounded text-lg align-content"
                 onClick={(e)=>handleSubmit(e)}>
                 Submit
             </button>
         </div>
         <div className="container mx-auto flex px-10 py-20 md:flex-row flex-col items-center" id="result">
-            <h1 className="title-font sm:text-4xl text-3xl mb-4 font-medium text-white items-center"
-            href="https://www.instagram.com/reel/CuPDDfUxOGZ/?utm_source=ig_web_copy_link&igshid=MzRlODBiNWFlZA==">
-                {name} You are {score}% gay.
+            <a href="https://www.instagram.com/reel/CuPDDfUxOGZ/?utm_source=ig_web_copy_link&igshid=MzRlODBiNWFlZA==">
+                <h1 className="title-font sm:text-4xl text-3xl mb-4 font-medium text-white align-content">
+                    {name} You are {score}% gay.
                 </h1>
+            </a>
         </div>
         {/* <div>
             <p>{count}</p>
