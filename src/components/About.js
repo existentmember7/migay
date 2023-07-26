162c85f242293664b00c9c37cda774e2a94a2d9c
韓維 <hanwei@hans-mac.lan>
2023-07-25 23:29:30 -0500

    init

--- src/components/About.js
+++ src/components/About.js
@@ -79,13 +79,13 @@ export default function About() {
           #EmbraceYourOrientation
           </p>
         </div>
-        {/* <div className="lg:max-w-lg lg:w-full md:w-1/2 w-5/6">
+        <div className="lg:max-w-lg lg:w-full md:w-1/2 w-5/6">
           <img
             className="object-cover object-center rounded"
             alt="hero"
-            src="./coding.svg"
+            src="./title.jpg"
           />
-        </div> */}
+        </div>
       </div>
       <div className="container mx-auto flex px-10 py-20 md:flex-row flex-col items-center">
         <h1 className="title-font sm:text-4xl text-3xl mb-4 font-medium text-white">
