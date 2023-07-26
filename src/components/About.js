1ac7f48107f42651b3d138cd448e434b445f5e70
韓維 <hanwei@hans-mac.local>
2023-07-25 20:10:05 -0500

    init

--- src/components/About.js
+++ src/components/About.js
@@ -7,7 +7,6 @@ export default function About() {
     const [name, setName] = React.useState("");
     const [email, setEmail] = React.useState("");
     const [message, setMessage] = React.useState("");
-    name = setName("Please enter your name");
 
     function encode(data) {
         return Object.keys(data)
@@ -21,7 +20,7 @@ export default function About() {
       <div className="container mx-auto flex px-10 py-20 md:flex-row flex-col items-center">
         <div className="lg:flex-grow md:w-1/2 lg:pr-24 md:pr-16 flex flex-col md:items-start md:text-left mb-16 md:mb-0 items-center text-center">
           <h1 className="title-font sm:text-4xl text-3xl mb-4 font-medium text-white">
-            Hi, 
+            Hi, \(Please enter your name below\)
             <input
               type="text"
               id="name"
