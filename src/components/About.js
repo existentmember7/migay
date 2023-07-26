ba1793a17e0e1e07164aa9b03e8ac5289451a120
韓維 <hanwei@hans-mac.local>
2023-07-25 19:57:35 -0500

    init

--- src/components/About.js
+++ src/components/About.js
@@ -2,18 +2,18 @@
 
 import React from "react";
 
-export default function About() {
-    const [name, setName] = React.useState("");
-    const [email, setEmail] = React.useState("");
-    const [message, setMessage] = React.useState("");
-  
-    function encode(data) {
-      return Object.keys(data)
-        .map(
-          (key) => encodeURIComponent(key) + "=" + encodeURIComponent(data[key])
-        )
-        .join("&");
-    }
+// export default function About() {
+//     const [name, setName] = React.useState("");
+//     const [email, setEmail] = React.useState("");
+//     const [message, setMessage] = React.useState("");
+
+//     function encode(data) {
+//         return Object.keys(data)
+//         .map(
+//             (key) => encodeURIComponent(key) + "=" + encodeURIComponent(data[key])
+//         )
+//         .join("&");
+//     }
 
 export default function About() {
   return (
@@ -21,13 +21,7 @@ export default function About() {
       <div className="container mx-auto flex px-10 py-20 md:flex-row flex-col items-center">
         <div className="lg:flex-grow md:w-1/2 lg:pr-24 md:pr-16 flex flex-col md:items-start md:text-left mb-16 md:mb-0 items-center text-center">
           <h1 className="title-font sm:text-4xl text-3xl mb-4 font-medium text-white">
-            Hi, <input
-              type="text"
-              id="name"
-              name="name"
-              className="w-full bg-gray-800 rounded border border-gray-700 focus:border-indigo-500 focus:ring-2 focus:ring-indigo-900 text-base outline-none text-gray-100 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"
-              onChange={(e) => setName(e.target.value)}
-            />
+            Hi, 
             <br className="hidden lg:inline-block" /> Is me what I think it is? Want to understand yourself even better? try this questionary.
             apps.
           </h1>
