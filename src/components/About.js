c454af4bee97840e577f22f297dc67c1c9302866
韓維 <hanwei@hans-mac.lan>
2023-07-26 03:17:44 -0500

    init

--- src/components/About.js
+++ src/components/About.js
@@ -294,7 +294,7 @@ export default function About() {
                 </label>
         </h1>
         </div>
-        <div className="container mx-auto flex px-10 py-0 md:flex-row flex-col items-center">
+        <div className="container mx-auto flex px-10 py-5 md:flex-row flex-col items-center">
             <button
                 type="submit"
                 className="text-white bg-indigo-500 border-0 py-2 px-6 focus:outline-none hover:bg-indigo-600 rounded text-lg justify-center"
@@ -302,7 +302,7 @@ export default function About() {
                 Submit
             </button>
         </div>
-        <div className="container mx-auto flex px-10 py-10 md:flex-row flex-col items-center">
+        <div className="container mx-auto flex px-10 py-5 md:flex-row flex-col items-center">
             <a href="https://www.instagram.com/reel/CuPDDfUxOGZ/?utm_source=ig_web_copy_link&igshid=MzRlODBiNWFlZA==">
                 <h1 className="title-font sm:text-4xl text-3xl mb-4 font-medium text-white justify-center">
                     {name} You are {score}% gay. {clickme}
